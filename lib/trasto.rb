963fa1ac3e2bf809cf3fe7c2a71d652c68997088
Jonathan Allard <jonathan@allard.io>
2014-11-25 19:42:26 -0500

    Add Trasto.locale override

--- lib/trasto.rb
+++ lib/trasto.rb
@@ -3,4 +3,9 @@ require 'trasto/translates'
 require 'trasto/class_methods'
 require 'trasto/instance_methods'
 module Trasto
+  def self.locale
+    defined?(@@locale) && @@locale
+  end
+
+  mattr_writer :locale
 end
