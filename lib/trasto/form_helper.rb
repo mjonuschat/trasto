5ac78fc4f6f62574e667957c9a8069547874246a
Jonathan Allard <jonathan@allard.io>
2015-11-19 13:18:38 -0500

    WIP: Make output buffer work
    [skip ci]

--- lib/trasto/form_helper.rb
+++ lib/trasto/form_helper.rb
@@ -1,5 +1,15 @@
 module Trasto::FormHelper
-  def fields_for_locale(locale, &block)
-    yield(self)
+  def self.included(base)
+    ActionView::Helpers::FormBuilder.include Trasto::FormHelper::Builder
+  end
+
+  def fields_for_locale(builder, locale, &block)
+    capture(builder, &block)
+  end
+
+  module Builder
+    def fields_for_locale(locale, &block)
+      @template.fields_for_locale(self, locale, &block)
+    end
   end
 end
--- spec/form_helper_spec.rb
+++ spec/form_helper_spec.rb
@@ -2,27 +2,27 @@ require 'action_view'
 
 describe Trasto::FormHelper do
   include ActionView::Helpers::FormHelper
-  include ActionView::RecordIdentifier
-  ActionView::Helpers::FormBuilder.include Trasto::FormHelper
+  include ActionView::Helpers::CaptureHelper
+  include Trasto::FormHelper
 
   def protect_against_forgery?; false; end
   attr_accessor :output_buffer
 
-  before :all do
+  before do
     @output_buffer = ""
   end
 
   describe "#fields_for_locale" do
     before do
-      form_for :post, url: "/posts" do |f|
+      concat(form_for(:post, url: "/posts") do |f|
         f.fields_for_locale :de do |g|
           g.text_field :title
         end
-      end
+      end)
     end
 
     it "sets the input name to nested locale" do
-      expect(output_buffer).to eq %{<input name="post[title][de]">}
+      expect(output_buffer).to include %{<input name="post[title][de]">}
     end
 
     it "assigns the existing field value"
